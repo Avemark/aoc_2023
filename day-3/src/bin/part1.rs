da72ca0b457d49a9858d052f67da5b368a116588
Christian <christian@avemark.se>
2023-12-04 22:52:03 +0100

    day 3. the last one was actually day 4.

---
+++ day-3/src/bin/part1.rs
@@ -0,0 +1,8 @@
+use day_3::part1::process;
+
+fn main() -> Result<()> {
+    let file = include_str!("../../input1.txt");
+    let result = process(file)?;
+    println!("{}", result);
+    Ok(())
+}
---
+++ day-3/src/bin/part2.rs
@@ -0,0 +1,8 @@
+use day_3::part2::process;
+
+fn main() -> Result<()> {
+    let file = include_str!("../../input2.txt");
+    let result = process(file)?;
+    println!("{}", result);
+    Ok(())
+}
